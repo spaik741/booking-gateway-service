e56ec592037a0281996c1c3ae8e0142e2db311e7
mihail <spaik741@gmail.com>
2025-10-03 17:31:35 +0300

    zipkin

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("io.zipkin.reporter2:zipkin-reporter-brave")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/com/bookinggatewayservice/BookingGatewayServiceApplication.kt
+++ src/main/kotlin/com/bookinggatewayservice/BookingGatewayServiceApplication.kt
@@ -3,11 +3,13 @@ package com.bookinggatewayservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import reactor.core.publisher.Hooks
 
 @EnableDiscoveryClient
 @SpringBootApplication
 class BookingGatewayServiceApplication
 
 fun main(args: Array<String>) {
+    Hooks.enableAutomaticContextPropagation()
     runApplication<BookingGatewayServiceApplication>(*args)
 }
